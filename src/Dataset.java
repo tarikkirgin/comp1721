6e325306b138df968ce042fc7f752e3c94497cbc
Tarik Kirgin <tarikkirgin@gmail.com>
2024-02-04 23:10:42 +0000

    add dataset skeleton

---
+++ src/Dataset.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Dataset
+{
+    private ArrayList<Double> data;
+
+    public Dataset(String filename)
+    {
+
+    }
+
+    public int size()
+    {
+
+    }
+
+    public double meanValue()
+    {
+
+    }
+}
