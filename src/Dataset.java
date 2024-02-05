65458924cb797c4902bf484639fdd3b053143ae3
Tarik Kirgin <tarikkirgin@gmail.com>
2024-02-05 09:46:15 +0000

    implement method code

--- src/Dataset.java
+++ src/Dataset.java
@@ -1,21 +1,40 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Dataset
 {
     private ArrayList<Double> data;
 
-    public Dataset(String filename)
-    {
 
+    public Dataset(String filename) throws IOException
+    {
+        File file = new File(filename);
+        Scanner fileReader = new Scanner(file);
+        while (fileReader.hasNextDouble())
+        {
+            data.add(fileReader.nextDouble());
+        }
     }
 
     public int size()
     {
-
+        return data.size();
     }
 
     public double meanValue()
     {
-
+        int arraySize = size();
+        if (arraySize == 0)
+        {
+            throw new ArithmeticException("No values in data array.");
+        }
+        double sum = 0;
+        for (double num : data)
+        {
+            sum += num;
+        }
+        return sum / arraySize;
     }
 }
